d6f1bf4811d58233a99f6a874cf15686e9ef209b
nosirjon05 <nosirjonkomilov05@gmail.com>
2022-07-27 11:50:11 +0500

    Add code

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var intro = alert("xush kelibsiz");
+var first_name = prompt("ismingiz nima", "Nosirjon");
+var last_name = prompt("familyangiz nima", "komilov");
+var young = prompt("necha yoshsiz", "13");
+var profession = prompt("qanday ishda ishlaysiz", "ishlamayman");
+var home = prompt("yashash joyingiz", "Toshkent");
+var confirm_info = confirm("ma'lumotlar uchun rahmat")
+
+console.log(`siz haqingizdagi malumotlar:
+${first_name }
+${last_name } 
+${young } 
+${profession } 
+${home } `);
